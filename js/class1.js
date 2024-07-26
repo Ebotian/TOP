db554dcc6100bf3ae59c1a47c6e49f912ce36fa9
Ebotian <yiboxiaotian@nuaa.edu.cn>
2024-07-26 22:21:34 +0800

    格式更正

--- js/class1.js
+++ js/class1.js
@@ -1,14 +1,14 @@
-let x = 30
-let y = 40
-let z="the result is "
-console.log(z + (x + y))
-let h = "100"
-let j = "200"
-console.log(h * j)
-let number = 2
-while(number!=Infinity){
-    number = number * number
-    console.log(number)
+let x = 30;
+let y = 40;
+let z = "the result is ";
+console.log(z + (x + y));
+let h = "100";
+let j = "200";
+console.log(h * j);
+let number = 2;
+while (number != Infinity) {
+	number = number * number;
+	console.log(number);
 }
 console.log("9" / 0);
 console.log(typeof Infinity);
@@ -19,6 +19,6 @@ console.log(myfloat);
 console.log(Number(myfloat.toFixed(3)));
 console.log(typeof myint);
 console.log(typeof myfloat);
-console.log((4 + 6 + 9) / 77)
-let a = 10
-console.log(a)
+console.log((4 + 6 + 9) / 77);
+let a = 10;
+console.log(a);
