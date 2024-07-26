96f5b60424961365006183b61abd2e8a5bb8cec2
Ebotian <yiboxiaotian@nuaa.edu.cn>
2024-07-26 22:12:19 +0800

    class1

---
+++ js/class1.js
@@ -0,0 +1,24 @@
+let x = 30
+let y = 40
+let z="the result is "
+console.log(z + (x + y))
+let h = "100"
+let j = "200"
+console.log(h * j)
+let number = 2
+while(number!=Infinity){
+    number = number * number
+    console.log(number)
+}
+console.log("9" / 0);
+console.log(typeof Infinity);
+const myint = 5;
+const myfloat = 6.666667;
+console.log(myint);
+console.log(myfloat);
+console.log(Number(myfloat.toFixed(3)));
+console.log(typeof myint);
+console.log(typeof myfloat);
+console.log((4 + 6 + 9) / 77)
+let a = 10
+console.log(a)
