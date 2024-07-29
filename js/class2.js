fe84bfb6cf3189492d2b800781a62ec077477370
Ebotian <yiboxiaotian@nuaa.edu.cn>
2024-07-29 19:37:01 +0800

    class2js

---
+++ js/class2.js
@@ -0,0 +1,18 @@
+function add7(num) {
+	return num + 7;
+}
+function multiply(num1, num2) {
+	return num1 * num2;
+}
+function capitalize(string) {
+	firstchar = string.charAt(0).toUpperCase();
+	restchar = string.slice(1).toLowerCase();
+	return firstchar + restchar;
+}
+function lastLetter(string) {
+	return string.slice(-1);
+}
+let myfun = (a, b) => a + b;
+
+let answer=parseInt(prompt("Enter a number:"));
+console.log(add7(answer));
